9626776bfef62ae963dff3ca5097e44e58d6b66d
Michael Leaney <mleaney@tokbox.com>
2016-06-30 18:18:40 +1000

    Add some log information

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
+import debug from 'debug';
+
+const log = debug('promise-each-concurrency');
+
 export async function promiseEach(iterable, fn, {
   concurrency = Infinity,
   progress = () => {},
 } = {}) {
   const nextItem = (function* next() {
     for (const x of iterable) {
+      log('retrieve next value in iterable');
       yield x;
     }
   }());
 
   function processNItems(n) {
     const promises = [];
+    log(`process ${n} items`);
     for (let i = 0; i < n; i++) {
       const next = nextItem.next();
+      log('got next item', next.done);
       if (next.done) { break; }
-      promises.push(fn(next.value));
+      const retVal = fn(next.value);
+      log('got the return value');
+      if (!retVal.then) {
+        log('does not look like you returned a promise!');
+      }
+      promises.push(retVal);
     }
 
     return Promise.all(promises);
   }
 
   while (true) { // eslint-disable-line no-constant-condition
+    log('processing some items');
     const items = await processNItems(concurrency);
+    log(`update progress ${items.length}`);
     if (items.length) { progress(items); }
+    log('finishing up');
     if (items.length < concurrency) { break; }
   }
 }
